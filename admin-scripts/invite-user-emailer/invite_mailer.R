bd9fdf372cbf6ee3bbe1aeb09b74af032d4481d3
Robert Shear <rshear@gmail.com>
2023-03-01 11:55:54 -0500

    Mods to email invtation code

--- admin-scripts/invite-user-emailer/invite_mailer.R
+++ admin-scripts/invite-user-emailer/invite_mailer.R
@@ -1,13 +1,14 @@
 # Create coresops.bioconductor.org invitations 
 library(jsonlite)
 library(tidyverse)
+library(lubridate)
 
 path_to_repo <- "~/bioc-core-sops"
 target_file <- "~/Downloads/invite_list.tsv"
 
 create_invitation <- function(UserId, Provider = "GitHub") {
   x <- str_glue(
-    "az staticwebapp users invite -n bioc-core-sops --authentication-provider {Provider} --user-details {UserId} --role verified --domain coresops.bioconductor.org --invitation-expiration-in-hours 120"
+    "az staticwebapp users invite -n bioc-core-sops --authentication-provider {Provider} --user-details {UserId} --role verified --domain coresops.bioconductor.org --invitation-expiration-in-hours 120 --output json"
     )
   fromJSON(system(x, intern = TRUE))
   
@@ -16,12 +17,13 @@ create_invitation <- function(UserId, Provider = "GitHub") {
 setwd(path_to_repo)
 
 coreteam <- fromJSON("data/members.json")
-list_users_cmd <- "az staticwebapp users list -n bioc-core-sops"
+list_users_cmd <- "az staticwebapp users list -n bioc-core-sops --output json"
 currentmemebers <- fromJSON(system(list_users_cmd, intern = TRUE))
 
 # Invite everyone who doesn't already have a github identity
 x <- coreteam |> 
   filter(!(GithubId %in% currentmemebers$displayName)) 
-x <- x |>  cbind(map_dfr(x$GithubId, create_invitation))
+x <- x |>  cbind(map_dfr(x$GithubId, create_invitation) |> mutate(name = NULL))
+x$expiresOn <- str_glue("{as.character(parse_datetime(x$expiresOn))} EST")
 
 write_tsv(x, target_file)
